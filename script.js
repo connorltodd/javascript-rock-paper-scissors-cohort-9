3b4104c7fb9c88d4e8ed4695acb49dd3717c7599
Connor Todd <connortodd0304@gmail.com>
2022-07-25 17:59:30 +0100

    implemented pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+// Pseudocode
+// 1. Group each game winning option into an object
+// options (array of objects)
+// scissors beats paper
+// paper beats rock
+// rock beats scissors
+// 2. a function to take the users choice (dom interaction) and store it inside a variable
+// 3. the computer will randomly select rock, paper or scissors 
+// (array which holds all the options, variable to choose the random number
+// array[randomNumber], store the computers choice in a variable, 
+// store the code inside a function)
+// 4. function to check who is the winner
+// 5. whoever the winner is increase the score 
+// 6. Reset the scores of each user back to zero 
